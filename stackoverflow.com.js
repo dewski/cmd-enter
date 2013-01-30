bff6af0e23a50ee97405d0696feee3c11d8f6a56
Jake Bellacera <hi@jakebellacera.com>
2013-01-30 15:54:41 -0800

    Adding stackoverflow.com to the readme
    * Removed textarea for dewski/cmd-enter#1

--- stackoverflow.com.js
+++ stackoverflow.com.js
@@ -1,4 +1,4 @@
-$(document).on('keydown', 'textarea#wmd-input, form[id^="add-comment-"] textarea', function(e) {
+$(document).on('keydown', '#wmd-input, form[id^="add-comment-"] textarea', function(e) {
   if(e.keyCode == 13 && (e.metaKey || e.ctrlKey)) {
     $(this).parents('form').submit();
   }
